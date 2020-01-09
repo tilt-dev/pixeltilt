ada40540a0a732770643771247c849cbc4bcb65e
L Körbes <ellenkorbes@gmail.com>
2020-01-09 19:48:02 +0100

    oh, the horrors

---
+++ main.go
@@ -0,0 +1,141 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/peterbourgon/diskv"
+)
+
+var d = diskv.New(diskv.Options{
+	BasePath:     "diskv",
+	Transform:    func(s string) []string { return []string{} },
+	CacheSizeMax: 1024 * 1024, // 1MB
+})
+
+func main() {
+	fmt.Println("\nStarting up!")
+	port := "8080"
+	if len(os.Args) > 1 {
+		port = os.Args[1]
+	}
+	http.HandleFunc("/", index)
+	http.HandleFunc("/upload", upload)
+	http.HandleFunc("/access/", access)
+	fmt.Println(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	// show index.html
+	absPath, err := filepath.Abs("./index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	file, err := ioutil.ReadFile(absPath)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Write(file)
+
+	// show current db entries
+	list := "<br>"
+	for key := range d.Keys(nil) {
+		list += "<a href='http://localhost:8080/access/" + key + "/'>" + key + "</a><br>"
+	}
+	w.Write([]byte(list))
+
+	// TODO: use proper templating? maybe?
+}
+
+func access(w http.ResponseWriter, r *http.Request) {
+	key := strings.Split(r.URL.Path, "/")[2]
+	image, err := d.Read(key)
+	if err != nil {
+		fmt.Println(err)
+	}
+	decoded, err := base64.StdEncoding.DecodeString(string(image))
+	if err != nil {
+		fmt.Println(err)
+	}
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(decoded)
+
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	// receive file
+	file, handler, err := r.FormFile("myFile")
+	if err != nil {
+		fmt.Println("Error Retrieving the File")
+		fmt.Println(err)
+		return
+	}
+	defer file.Close()
+	fmt.Printf("Uploaded File: %+v\tFile Size: %+v\tMIME: %+v\n", handler.Filename, handler.Size, handler.Header)
+
+	// create a temp file
+	tempFile, err := ioutil.TempFile("", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// save to temp file
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		fmt.Println(err)
+	}
+	tempFile.Write(fileBytes)
+	fmt.Println(tempFile.Name())
+	defer os.Remove(tempFile.Name())
+
+	// enhance!
+	enhanced, err := enhance(tempFile.Name())
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer os.Remove(enhanced)
+
+	// serve output
+	output, err := os.Open(enhanced)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileBytes, err = ioutil.ReadAll(output)
+	w.Header().Set("Content-Type", "image/png")
+	w.Write(fileBytes)
+
+	// save to db
+	encoded := base64.StdEncoding.EncodeToString(fileBytes)
+	d.Write(time.Now().Format("2006-01-02-15-04-05"), []byte(encoded))
+	output.Close()
+
+	// TODO: do all of this in memory instead of writing temp files left and right
+}
+
+func enhance(file string) (string, error) {
+	inputFile := fmt.Sprintf("image=@%s;type=image/png", file)
+	outputFile, err := ioutil.TempFile("", "enhanced-*.png")
+	if err != nil {
+		return "", err
+	}
+	outputFile.Close()
+	// using curl because I forgot how to do this in native go
+	cmdOutput, err := exec.Command("curl", "-X", "POST", "http://localhost:5000/model/predict", "-H", "accept: application/json", "-H", "Content-Type: multipart/form-data", "-F", inputFile, "--output", outputFile.Name(), "-s").CombinedOutput()
+	if err != nil {
+		return "", err
+	}
+	fmt.Printf("curl output: %s\n", cmdOutput)
+	return outputFile.Name(), nil
+
+	// TODO: rewrite in go instead of relying on curl
+}
