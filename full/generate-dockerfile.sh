3688b758cd2747c558c6c7bde8ede85ee5d8485a
Dan Bentley <dan@tilt.dev>
2021-07-21 14:21:17 -0400

    workshop demo

--- full/generate-dockerfile.sh
+++ full/generate-dockerfile.sh
@@ -3,9 +3,9 @@
 read -r -d '' VAR <<- EOM
 FROM alpine
 RUN apk add entr
-RUN mkdir /app 
-WORKDIR /app 
+RUN mkdir /app
+WORKDIR /app
 ADD $files ./
-CMD ls main | entr -r ./main
+CMD ls main | entr -n -r ./main
 EOM
 echo "$VAR"
